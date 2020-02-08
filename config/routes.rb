ea7be8fe1aa464868676bb3420f39fd90f7e0b5f
Katie Hinkle <katie@Katies-MacBook-Air.local>
2020-02-08 11:42:09 -0800

    added pages controller, views, and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  #root to: "pages#index"
+  root to: "pages#index"
+
   resources :movies
   resources :users
   resources :comments
@@ -11,5 +12,5 @@ Rails.application.routes.draw do
 
   get "/signup", to: "users#new" 
   post "/signup", to: "users#create"
-  
+
 end
