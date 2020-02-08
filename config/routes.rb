da76875e1a2c465956ad31dc76e7e97f33b292cd
Katie Hinkle <katie@Katies-MacBook-Air.local>
2020-02-08 11:05:20 -0800

    Added login and signup routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,12 @@ Rails.application.routes.draw do
   resources :movies
   resources :users
   resources :comments
+
+  get "/login", to: "session#new"
+  post "/login", to: "session#create" 
+  delete "/logout", to: "session#destroy"
+
+  get "/signup", to: "users#new" 
+  post "/signup", to: "users#create"
+  
 end
