85e4814303054271455aac5113ebcdcbd1ac1ee9
Katie Hinkle <katie@Katies-MacBook-Air.local>
2020-02-08 13:38:55 -0800

    fixed session controller

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -5,7 +5,7 @@ class SessionController < ApplicationController
     def create
         user = User.find_by(email: params[:email])
         
-        if user && user.authenticate(params[:password_digest]) 
+        if user && user.authenticate(params[:password]) 
             session[:user_id] = user.id.to_s 
             redirect_to(root_path, notice: "Logged in!")
         else
