8326b8879e8850dea5ad2e277f79164a3fdf1d7d
Katie Hinkle <katie@Katies-MacBook-Air.local>
2020-02-08 11:33:07 -0800

    Added session view and fixed session_controller so it doesn't throw an error

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -5,7 +5,9 @@ class SessionController < ApplicationController
     def create
         user = User.find_by(email: params[:email])
         
-        if user && user.authenticate(params[:password]) session[:user_id] = user.id.to_s redirect_to(root_path, notice: "Logged in!")
+        if user && user.authenticate(params[:password]) 
+            session[:user_id] = user.id.to_s 
+            redirect_to(root_path, notice: "Logged in!")
         else
             render(:new) 
         end
